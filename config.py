7b6e65eafcc3972293d3af86f1b5a6695b3b86f1
Jaime Barranco <jaimebarran@gmail.com>
2025-06-27 15:52:55 +0200

    upd: move config to env

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ ALLOWED_EXTENSIONS = {'gz', 'zip', '7z'}
     
 class Config:
     # Flask secret key
-    SECRET_KEY = os.getenv('SECRET_KEY', 'ALongRandomlyGeneratedString')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     
     # Auth0
-    AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID', 'YOUR_AUTH0_CLIENT_ID')
-    AUTH0_CLIENT_SECRET = os.getenv('AUTH0_CLIENT_SECRET', 'YOUR_AUTH0_CLIENT_SECRET')
-    AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN', 'dev-efo7i5wwqsmfsqvt.eu.auth0.com')
-    AUTH0_AUDIENCE = os.getenv('AUTH0_AUDIENCE', 'https://dev-efo7i5wwqsmfsqvt.eu.auth0.com/api/v2/')
-    AUTH0_CALLBACK_URL = os.getenv('AUTH0_CALLBACK_URL', 'http://localhost:5000/callback')
+    AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID')
+    AUTH0_CLIENT_SECRET = os.getenv('AUTH0_CLIENT_SECRET')
+    AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+    AUTH0_AUDIENCE = os.getenv('AUTH0_AUDIENCE')
+    AUTH0_CALLBACK_URL = os.getenv('AUTH0_CALLBACK_URL')
 
     # Mail config
     MAIL_SERVER = os.getenv('MAIL_SERVER')
