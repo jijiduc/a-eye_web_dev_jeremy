8a216c26e3cfb2435cb0a056b2d2da4adb91a047
jijiduc <jijiduc23@gmail.com>
2026-06-01 15:45:00 +0200

    feat : update segmentation process to user-scope

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
 import os
 import subprocess
 from datetime import datetime
-from utils import *
-from config import *
+from utils import modify_jobfile, print_and_log, copy_file_to_hpc, copy_files_from_hpc, user_reference_rmtree, move_file, copy_folder
+from config import JOBFILE_HPC, SSH_USER, LOGS_FOLDER, JOBFILE_TEMPLATE, OUTPUT_HPC
+from models import UserPaths
 
 # ----------------------------------------------------------------------------------------------
 # MAIN FUNCTION
 
-def getSegmentation(output=None, user_email=None):
-    ''' Main function: performs inference on input folder and saves output in output folder. '''
+def getSegmentation(user_email: str, paths: UserPaths) -> str:
+    """Main function : performs inference on input folder and saves output in output folder
 
-    # paths
-    output = output or DOWNLOAD_FOLDER
-    output_hpc = OUTPUT_HPC
-    template = JOBFILE_TEMPLATE
-    jobfile = JOBFILE
-    jobfile_hpc = JOBFILE_HPC
-    
+    Args:
+        user_email (str): user's email address
+        paths (UserPaths): the generated paths based on the user's adress
+
+    Returns:
+        str: a message to announce segmentation done
+    """
     # We would need this for copying the resulting files from the HPC to the local machine
     # Make the email safe for paths
-    safe_email = re.sub(r'[@.]', '_', user_email)
-    # Timestamp
+    safe_email = user_email.replace("@", "_at_").replace(".", "_")
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    # modify jobfile
-    modify_jobfile(template, user_email, timestamp, jobfile)
-        
-    # copy jobfile to hpc
-    copy_file_to_hpc(jobfile, os.path.dirname(jobfile_hpc))
+    jobfile     = paths.jobfile
+    jobfile_hpc = f"{os.path.dirname(JOBFILE_HPC)}/nnunet_inference_{safe_email}.sh"
 
+    modify_jobfile(JOBFILE_TEMPLATE, user_email, timestamp, jobfile, paths.hpc_input)
+    copy_file_to_hpc(jobfile, os.path.dirname(jobfile_hpc))
     # inference command terminal (nnUNet)
     inference_command = f'ssh {SSH_USER} "sbatch --wait --partition=Dance --account=mattech --qos=normal {jobfile_hpc}"'
     print_and_log("[A-eye] Submitted batch job to HPC...", 'info', LOGS_FOLDER)
     subprocess.run(inference_command, shell=True, check=True)
+
+    copy_files_from_hpc(f'{OUTPUT_HPC}/{safe_email}_{timestamp}', paths.download)
+    # Logs handling
+    output_logs = paths.download / 'logs'
+    if output_logs.exists():
+        user_reference_rmtree(output_logs)
+    output_logs.mkdir(parents=True, exist_ok=True)
+    move_file(str(paths.download / '*.err'), output_logs)
+    move_file(str(paths.download / '*.out'), output_logs)
     
-    # copy output folder from hpc
-    copy_files_from_hpc(f'{output_hpc}/{safe_email}_{timestamp}', output)
-    output_logs = os.path.join(output, 'logs')
-    if os.path.exists(output_logs):
-        safe_rmtree(output_logs)
-    os.makedirs(output_logs, exist_ok=True)
-    move_file(f'{output}/*.err', output_logs)  # move error files to logs folder
-    move_file(f'{output}/*.out', output_logs)  # move output files to logs folder
-    
-    # Copy logs
-    copy_folder(LOGS_FOLDER, output_logs)  # needed for app.log
+    copy_folder(LOGS_FOLDER, output_logs)
 
     return '\nInference finished!!'
